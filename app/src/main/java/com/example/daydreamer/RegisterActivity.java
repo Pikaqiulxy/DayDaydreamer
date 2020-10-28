85fa1e742136660728313e3483b98e0da1de3b47
luoxinyue <244880572@qq.com>
2020-10-28 16:56:33 +0800

    实现注册功能并自动分配id

--- app/src/main/java/com/example/daydreamer/RegisterActivity.java
+++ app/src/main/java/com/example/daydreamer/RegisterActivity.java
@@ -2,47 +2,43 @@ package com.example.daydreamer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.daydreamer.ui.login.LoginActivity;
 
-import java.io.File;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.sql.Types.NULL;
+import java.util.Random;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
     ImageView iid;
-    TextView textView;
     EditText name;
-    EditText uid;
     EditText pw;
     EditText pw2;
     EditText note;
     Button rbutton;
     String sname,suid,spw,spw2,snote;
-    int iii=0 ,jjj=0;
+    int iuid,j=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,115 +54,115 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         iid = findViewById(R.id.iid);
-        textView = findViewById(R.id.textView);
         name = findViewById(R.id.name);
-        uid = findViewById(R.id.uid);
         pw = findViewById(R.id.pw);
         pw2 = findViewById(R.id.pw2);
         note = findViewById(R.id.note);
-        rbutton = findViewById(R.id.rbutton);
-
-        sname = name.getText().toString();
-        suid = uid.getText().toString();
-        spw = pw.getText().toString();
-        spw2 = pw2.getText().toString();
-        snote = note.getText().toString();
-
+        rbutton = (Button)findViewById(R.id.rbutton);
 
         final TextView gologin = findViewById(R.id.gologin);
+
+        final Thread thread = new Thread(new Runnable() {  //连接数据库
+            @Override
+            public void run() {
+                //1.判断输入值是否为空
+
+
+                // 2.设置好IP/端口/数据库名/用户名/密码等必要的连接信息
+                String ip = "39.97.166.131";
+                int port = 3306;
+                String dbName = "daydb";
+                String url = "jdbc:mysql://" + ip + ":" + port
+                        + "/" + dbName; // 构建连接mysql的字符串
+                String user = "daydreamer";
+                String password = "123456";
+
+                // 3.连接JDBC
+                Connection conn = null;
+                try {
+                    conn = DriverManager.getConnection(url, user, password);
+                    Log.i(TAG, "远程连接成功!");
+                } catch (SQLException e) {
+                    Log.e(TAG, "远程连接失败!");
+                }
+
+                if (conn != null) {
+                    try {
+                        // 创建用来执行sql语句的对象
+                        java.sql.Statement statement = conn.createStatement();
+                        //生成不重复的 min<=uid<max
+                        while (j==0){
+                            int min = 1;
+                            int max = 10000000;
+                            iuid = min + ((int) (new Random().nextFloat() * (max - min)));
+                            suid = Integer.toString(iuid);
+                            //查询语句
+                            String sql = "select uid from users_d where uid = '"+suid+"'";
+                            // 执行sql查询语句并获取查询信息
+                            ResultSet rSet = statement.executeQuery(sql);
+                            //判断结果集是否为空
+                            if(rSet.next()){
+                                j=0;
+                            }
+                            else j=1;
+                        }
+
+                        // 迭代打印出查询信息
+                        /*
+                        while (rSet.next()) {
+                            List<String> list = new ArrayList<String>();//创建取结果的列表，之所以使用列表，不用数组，因为现在还不知道结果有多少，不能确定数组长度，所有先用list接收，然后转为数组
+                            list.add(rSet.getString("uid"));
+                            if (list != null && list.size() > 0) {//如果list中存入了数据，转化为数组
+                                String[] arr = new String[list.size()];//创建一个和list长度一样的数组
+                                for (int i = 0; i < list.size(); i++) {
+                                    arr[i] = list.get(i);//数组赋值了。
+                                }
+                                Log.i(TAG, "输出j:");
+                            }
+                        }
+                        */
+
+                        //输入语句
+                        String sql1 = "insert into users_d(iid,name,uid,pw,note) values(NULL,'"+sname+"','"+suid+"','"+spw+"','"+snote+"')";//数据库语句
+                        //执行sqlINSERT、UPDATE 或 DELETE 语句
+                        statement.executeUpdate(sql1);
+                        Intent intent = new Intent();
+                        intent.setClass(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "关闭连接失败");
+                }
+            }
+        });
 
         rbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(sname != null){
-                    if(suid != null){
-                        if(snote != null){
-                            if(spw !=null){
-                                if(spw.equals(spw2)){
-                                    iii = 1;
-                                }else textView.setText("The password input is inconsistent");
-                            }else textView.setText("pleaseSetYourPassword");
-                        }else textView.setText("PleaseSetYourIntroduction");
-                    }else textView.setText("PleaseSetYourOnlyId");
-                }else textView.setText("PleaseSetYourName");
 
+                sname = name.getText().toString();
+                spw = pw.getText().toString();
+                spw2 = pw2.getText().toString();
+                snote = note.getText().toString();
 
-                Intent intent = new Intent();
-                intent.setClass(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
+                if(!sname.equals("")){
+                    if(!snote.equals("")){
+                        if(!spw.equals("")){
+                            if(spw.equals(spw2)){
+                                thread.start();
+                            }else Toast.makeText(RegisterActivity.this,"The password input is inconsistent",Toast.LENGTH_LONG).show();
+                        }else Toast.makeText(RegisterActivity.this,"pleaseSetYourPassword",Toast.LENGTH_LONG).show();
+                    }else Toast.makeText(RegisterActivity.this,"PleaseSetYourIntroduction",Toast.LENGTH_LONG).show();
+                }else Toast.makeText(RegisterActivity.this,"PleaseSetYourName",Toast.LENGTH_LONG).show();
             }
         });
-
-            final Thread thread = new Thread(new Runnable() {  //连接数据库
-                @Override
-                public void run() {
-                    if(iii == 1){
-                    // 2.设置好IP/端口/数据库名/用户名/密码等必要的连接信息
-                    String ip = "39.97.166.131";
-                    int port = 3306;
-                    String dbName = "daydb";
-                    String url = "jdbc:mysql://" + ip + ":" + port
-                            + "/" + dbName; // 构建连接mysql的字符串
-                    String user = "daydreamer";
-                    String password = "123456";
-
-                    // 3.连接JDBC
-                    Connection conn = null;
-                    try {
-                        conn = DriverManager.getConnection(url, user, password);
-                        Log.i(TAG, "远程连接成功!");
-                    } catch (SQLException e) {
-                        Log.e(TAG, "远程连接失败!");
-                    }
-
-                    if (conn != null) {
-                        //查询语句
-                        String sql = "select uid from users_d";
-
-                        try {
-                            // 创建用来执行sql语句的对象
-                            java.sql.Statement statement = conn.createStatement();
-                            //执行sqlINSERT、UPDATE 或 DELETE 语句
-                            //statement.executeUpdate(sql1);
-                            // 执行sql查询语句并获取查询信息
-                            ResultSet rSet = statement.executeQuery(sql);
-                            // 迭代打印出查询信息
-
-                            while (rSet.next()) {
-                                //Log.i(TAG, rSet.getString("iid") + "输出" + rSet.getString("name") + "\t");
-                                List<String> list = new ArrayList<String>();//创建取结果的列表，之所以使用列表，不用数组，因为现在还不知道结果有多少，不能确定数组长度，所有先用list接收，然后转为数组
-                                list.add(rSet.getString(1));
-                                if (list != null && list.size() > 0) {//如果list中存入了数据，转化为数组
-                                    String[] arr = new String[list.size()];//创建一个和list长度一样的数组
-                                    for (int i = 0; i < list.size(); i++) {
-                                        arr[i] = list.get(i);//数组赋值了。
-                                    }
-                                    //遍历比较uid是否唯一
-                                    for (int i = 0; i < arr.length; i++) {
-                                        if (arr[i].equals(spw)) {
-                                            textView.setText("This ID is already in use");
-                                            jjj = 1;
-                                            break;
-                                        }
-                                    }
-
-                                }
-                            }
-                            if(jjj == 0){
-                                //输入语句
-                                String sql1 = "insert into users_d(iid,name,uid,pw,note) values(NULL,'"+sname+"','"+suid+"','"+spw+"','"+snote+"')";//数据库语句
-                            }
-                            conn.close();
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                            Log.e(TAG, "关闭连接失败");
-                        }
-                    }
-                    }
-                }
-            });
-            thread.start();
-
 
         gologin.setOnClickListener(new View.OnClickListener() {
             @Override
